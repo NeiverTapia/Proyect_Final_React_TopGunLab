39a08457473631a2af296a873764f80ebba0a693
Neiver97 <neiver.tapia@udea.edu.co>
2019-06-22 08:14:49 -0500

    agrego vista en otra carpeta, y conecto 1 componente

---
+++ proyectfinal_top_gun_lab_react/src/components/Greet.js
@@ -0,0 +1,14 @@
+import React from 'react'
+/*
+function Greet(){
+    return <h1> Hello World, welcome to your first page with react ¡Neiver!</h1>
+}
+export default Greet;
+*/
+
+export const Greet=() => 
+<div>
+    <h1>Hello World, welcome to your first page with react ¡Neiver!</h1>
+    <p>En este apartado estoy haciendo una prueba</p>;  
+</div>
+
